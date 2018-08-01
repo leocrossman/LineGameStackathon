cfc40184e0813547525819b5e77eb883497efd2c
leocrossman <lkcrossman@gmail.com>
2018-08-01 17:00:18 -0400

    added tensorflow via script tag

--- public/main.js
+++ public/main.js
@@ -1,14 +1,15 @@
-let xs = [];
-let ys = [];
+const xs = [];
+const ys = [];
+
+let m, b; // slope and y-intercept
 
 function setup() {
   createCanvas(800, 800);
-  background(0);
 }
 
 function mousePressed() {
-  let x = map(mouseX, 0, width, 0, 1); // normlaizes our plane to be in the first quadrant instead of just the "width and height"
-  let y = map(mouseY, 0, height, 1, 0);
+  const x = map(mouseX, 0, width, 0, 1); // normlaizes our plane to be in the first quadrant instead of just the "width and height"
+  const y = map(mouseY, 0, height, 1, 0);
   xs.push(x);
   ys.push(y);
 }
@@ -18,8 +19,8 @@ function draw() {
   stroke(255);
   strokeWeight(Math.floor(width / 100)); // Sets the dot sizes to be 1/100th the size of the screen
   for (let i = 0; i < xs.length; i++) {
-    let px = map(xs[i], 0, 1, 0, width); // maps from our normalized quadrant back to canvas coords
-    let py = map(ys[i], 0, 1, height, 0);
+    const px = map(xs[i], 0, 1, 0, width); // maps from our normalized quadrant back to canvas coords
+    const py = map(ys[i], 0, 1, height, 0);
     point(px, py);
   }
 }
