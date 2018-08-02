ff9593fb44c9c71fc3224dc7e59c924bf6aa42ec
leocrossman <lkcrossman@gmail.com>
2018-08-02 12:01:15 -0400

    line works but needs to be tidy-d

--- README.md
+++ README.md
@@ -24,14 +24,17 @@
 
     * Optimizer
       - **Allows** us to minimize the Loss Function via a Learning Rate
+      - FROM THE TF DOCS: Executes the loss function and minimizes the scalar output of the loss function by computing gradients of y with respect to the list of trainable variables provided by varList. If no list is provided, it defaults to all trainable variables.
+
+"
 
     - Train
       - Minimize the loss function with the optimizer, adjusting m and b based on the loss function
 
-  * Process
+- Process
 
-    1.  Get the data.
-    2.  Define loss function.
-        - Parameters _m_ and _b_ from equation of a line are our parameters that we pass to the loss function that allow us to create the predictions on our line to compare with the actual points from our data.
-    3.  Define optimizer.
-    4.  **Optimizer minimizes the loss function via the learning rate.**
+  1.  Get the data.
+  2.  Define loss function.
+      - Parameters _m_ and _b_ from equation of a line are our parameters that we pass to the loss function that allow us to create the predictions on our line to compare with the actual points from our data.
+  3.  Define optimizer.
+  4.  **Optimizer minimizes the loss function via the learning rate.**
--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ const yVals = [];
 
 let m, b; // slope and y-intercept
 
-const learningRate = 0.2; // how quickly the optimizer brings the line to the correct place
+const learningRate = 0.02; // how quickly the optimizer brings the line to the correct place
 const optimizer = tf.train.sgd(learningRate);
 
 function setup() {
@@ -35,21 +35,38 @@ function mousePressed() {
 function predict(x) {
   const xs = tf.tensor1d(x);
   // y = mx + b
-  const ys = tfxs.mul(m).add(b);
+  const ys = xs.mul(m).add(b);
 
   return ys;
 }
 
 function draw() {
-  const ys = tf.tensor1d(yVals);
-  optimizer.minimize(() => loss(predict(xVals), ys)); // minimize the loss function
-
+  // Since we made m and b tf.variables, we are able to adjust those tensors
+  if (xVals.length > 0) {
+    const ys = tf.tensor1d(yVals); // transform ys from array to tensor
+    optimizer.minimize(() => loss(predict(xVals), ys)); // minimize the loss function
+  }
   background(0);
   stroke(255);
   strokeWeight(Math.floor(width / 100)); // Sets the dot sizes to be 1/100th the size of the screen
-  for (let i = 0; i < xs.length; i++) {
-    const px = map(xs[i], 0, 1, 0, width); // maps from our normalized quadrant back to canvas coords
-    const py = map(ys[i], 0, 1, height, 0);
+  for (let i = 0; i < xVals.length; i++) {
+    const px = map(xVals[i], 0, 1, 0, width); // maps from our normalized quadrant back to canvas coords
+    const py = map(yVals[i], 0, 1, height, 0);
     point(px, py);
   }
+
+  let xs = [0, 1];
+  const ys = predict(xs);
+
+  // xs are floats so they can be used in the line func
+  let x1 = map(xs[0], 0, 1, 0, width);
+  let x2 = map(xs[1], 0, 1, 0, width);
+
+  // ys are tensors so we have to get floats from them with dataSync
+  let lineY = ys.dataSync(); // dataSync is an async func so could slow performance but we should be okay...
+  let y1 = map(lineY[0], 0, 1, height, 0);
+  let y2 = map(lineY[1], 0, 1, height, 0);
+
+  strokeWeight(Math.floor(width / 200));
+  line(x1, y1, x2, y2);
 }
