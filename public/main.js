9971b8eaf4853cabeff9f9783fbef21c0308b533
Ben Reitman <ben.reitman@gmail.com>
2018-08-02 12:31:50 -0400

    made title screen

--- public/main.js
+++ public/main.js
@@ -6,6 +6,8 @@ let m, b; // slope and y-intercept
 const learningRate = 0.2; // how quickly the optimizer brings the line to the correct place
 const optimizer = tf.train.sgd(learningRate);
 
+let start = false;
+
 //FOR PLAYER
 let xCoord = 30;
 let yCoord = 40;
@@ -13,7 +15,6 @@ const player = new Player(xCoord, yCoord, 30, 30);
 
 function setup() {
   createCanvas(800, 800);
-
   // tf.variable allows us to change m and b. Tensors are immutable otherwise
   // tf.scalar because we are using tensors. `scalar` just means integer.
   m = tf.variable(tf.scalar(random(1))); // random number between 0 and 1
@@ -37,12 +38,29 @@ function predict(x) {
 
 function draw() {
   background(0);
-  stroke(255);
-  player.display();
-  strokeWeight(Math.floor(width / 100)); // Sets the dot sizes to be 1/100th the size of the screen
-  for (let i = 0; i < xs.length; i++) {
-    const px = map(xs[i], 0, 1, 0, width); // maps from our normalized quadrant back to canvas coords
-    const py = map(ys[i], 0, 1, height, 0);
-    point(px, py);
+  if(start === false){
+    stroke(255);
+    fill(255);
+    textAlign(CENTER, CENTER)
+    textSize(40);
+    text('LINE GAME', width/2, height/2 - Math.floor(width/16));
+    textSize(30);
+    text('Press Enter to Start', width/2, height/2 );
+  }
+  else{
+    stroke(255);
+    player.display();
+    strokeWeight(Math.floor(width / 100)); // Sets the dot sizes to be 1/100th the size of the screen
+    for (let i = 0; i < xs.length; i++) {
+      const px = map(xs[i], 0, 1, 0, width); // maps from our normalized quadrant back to canvas coords
+      const py = map(ys[i], 0, 1, height, 0);
+      point(px, py);
+    }
+  }
+}
+
+function keyPressed(){
+  if(keyCode == ENTER){
+    start = true;
   }
 }
